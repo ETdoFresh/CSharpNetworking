632efb23f31865b28b05950dbef0beb97ff8a71d
ETdoFresh <ETdoFresh@gmail.com>
2024-02-17 10:37:37 -0600

    Remove unused using statements

--- ExampleWebSocketClient/Program.cs
+++ ExampleWebSocketClient/Program.cs
@@ -1,9 +1,5 @@
 ﻿using CSharpNetworking;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExampleWebSocketClient
 {
