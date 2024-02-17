04871acff92cea2296577ef15cd60dca68fadd13
ETdoFresh <ETdoFresh@gmail.com>
2024-02-17 16:07:17 -0600

    Refactor WebSocket client code

--- ExampleWebSocketClient/Program.cs
+++ ExampleWebSocketClient/Program.cs
@@ -5,29 +5,32 @@ using System.Threading.Tasks;
 
 namespace ExampleWebSocketClient
 {
-    class Program
+    public static class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var uri = "wss://echo.websocket.org";
+            
             Console.WriteLine($"This is an example WebSocket Client. Press any key to connect to {uri}");
             Console.ReadKey();
 
             var client = new WebSocketClient(uri);
             client.Opened += () => { Console.WriteLine("Connected to server."); };
-            client.Opened += () => { client.SendAsync("Hello World!"); };
+            client.Closed += () => { Console.WriteLine("Disconnected from server."); };
+            client.Error += e => { Console.WriteLine($"Error: {e.Message}"); };
             client.Received += bytes => { Console.WriteLine($"Received from server: {Encoding.UTF8.GetString(bytes)}"); };
             client.Sent += bytes => { Console.WriteLine($"Sent to server: {Encoding.UTF8.GetString(bytes)}"); };
-            client.Closed += () => { Console.WriteLine("Disconnected from server."); };
+
+            Console.WriteLine("Submit 'exit' command to stop the client.");
             
-            Console.WriteLine("Connecting...");
-            await client.OpenAsync();
-            _ = client.SendAsync("Hello World 2!");
-            _ = client.SendAsync("Hello World 3!");
+            _ = client.OpenAsync();
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "exit") break;
+                _ = client.SendAsync(input);
+            }
             _ = client.CloseAsync();
-            
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
         }
     }
 }
